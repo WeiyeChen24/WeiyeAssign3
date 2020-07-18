5f900aa0c59512f53c6b6d75c11dbdd28692e58f
WeiyeChen24 <68452004+WeiyeChen24@users.noreply.github.com>
2020-07-17 23:23:36 -0400

    Final Commit Weiye Chen

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -7,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent{
 
-  sidenum;
+  side;
 
   constructor(){
     this.getRandomDice();
   }
   
   getRandomDice(){
-    this.sidenum = Math.floor(Math.random() * 6 + 1);
+    this.side = Math.floor(Math.random() * 6 + 1);
   }
 
 }
