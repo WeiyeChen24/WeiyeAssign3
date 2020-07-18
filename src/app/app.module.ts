9c9e29554e3c19f68002bfcb1e975f43b74e71ac
WeiyeChen24 <68452004+WeiyeChen24@users.noreply.github.com>
2020-07-17 22:09:48 -0400

    Add content,header,footer
    Add new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
